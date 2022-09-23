730a76cc344d5c92a639576b4cd425c8864a09a8
LuWo0 <loviedo46@yahoo.com>
2022-09-22 19:39:45 -0700

    boilderplate stuff done

---
+++ minecraft-clone/src/images/images.js
@@ -0,0 +1,15 @@
+import dirtImg from "dirt.jpeg";
+import glassImg from "glass.png";
+import grassImg from "grass.jpeg";
+import logImg from "log.jpeg";
+import woodImg from "wood.png";
+
+const images = [
+    dirtImg,
+    glassImg,
+    grassImg,
+    logImg,
+    woodImg
+];
+
+export default images;
