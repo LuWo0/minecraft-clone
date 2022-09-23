1fb0080520cc0276f8b9ce23a9a34c9d3f16f0ae
LuWo0 <loviedo46@yahoo.com>
2022-09-22 21:45:24 -0700

    added fpv and a cursor

--- minecraft-clone/src/App.jsx
+++ minecraft-clone/src/App.jsx
@@ -3,18 +3,20 @@ import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/cannon";
 import { Ground } from "./components/Ground";
 import { Player } from "./components/Player";
-
+import { FirstPersonView } from "./components/FirstPersonView";
 function App() {
   return (
     <>
       <Canvas>
         <Sky sunPosition={[100, 100, 20]}/>
         <ambientLight intensity={0.5} />
+        <FirstPersonView />
         <Physics>
           <Player />
           <Ground />
         </Physics>
       </Canvas>
+      <div className="absolute  centered cursor">+</div>
     </>
   )
 }
